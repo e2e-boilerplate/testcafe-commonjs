7fb82c86c217775834365d7f87c612450e428a80
Girma Nigusse <xgirma@gmail.com>
2020-06-27 07:46:41 -0400

    updates

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -3,7 +3,5 @@ const { Selector } = require("testcafe");
 fixture`Sandbox`.page`https://www.google.com/`;
 
 test("should be on Sandbox", async (t) => {
-  await t
-    .expect(Selector("title").innerText)
-    .eql("Google");
+  await t.expect(Selector("title").innerText).eql("Google");
 });
