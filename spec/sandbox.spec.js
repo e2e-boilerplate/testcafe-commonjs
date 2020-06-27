926783e0b9b7c262cfb95aed64f521a11801a151
Girma Nigusse <xgirma@gmail.com>
2020-06-27 07:45:52 -0400

    updates

--- spec/sandbox.spec.js
+++ spec/sandbox.spec.js
@@ -5,7 +5,5 @@ fixture`Sandbox`.page`https://www.google.com/`;
 test("should be on Sandbox", async (t) => {
   await t
     .expect(Selector("title").innerText)
-    .eql("Google")
-    .expect(Selector("h1").innerText)
     .eql("Google");
 });
